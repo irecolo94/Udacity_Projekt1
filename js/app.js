ba3328d0d57769c047f9043a8ad6c08ec8bd309b
irecolo94 <ireblue@virgilio.it>
2021-01-10 12:26:57 +0100

    feat: improved the click events for nav items

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ const navbar = document.getElementById('navbar_content');
 const navID = document.getElementById('nav');
 const secs = document.querySelectorAll('section');
 
-
 // **DYNAMIC NAVIGATION**
 function updateNav() {
-    let list = "";
+  // following "let" will be updated with list content
+  let list = "";
+  // loop over each section to create a corrisponding nav item
   secs.forEach((section) => {
     const secId = section.id;
     const secMenuName = section.dataset.menuname;
@@ -14,12 +15,13 @@ function updateNav() {
     const toUpdate = '<li class="nav_item">' + '<a data-page="' + secMenuName + '" ' + 'data-sectionclass ="' + secClass + '" href="#' + secId + '">' + '<h4>' + secMenuName + '</h4>' + '</a>' + '</li>';
     list += toUpdate;
   });
-    navbar.innerHTML += list;
+  // update innerHTML with content
+  navbar.innerHTML += list;
 };
 
 updateNav();
 
-// **MARK ITEMS AS ACTIVE IF YOU CLICK ON THEM**
+// **MARK ITEMS AS ACTIVE ON NAV IF YOU CLICK ON THEM**
 //consts for active clicked items
 const navbarItems = navbar.querySelectorAll('li');
 const alreadyActive = document.getElementsByClassName('activebox');
@@ -47,11 +49,11 @@ navLinks.forEach(link => {
   });
 });
 
-// ** ACTIVE STATES FOR BAR AND SECS IF SEC IS IN VIEWPORT
+// ** ACTIVE STATES FOR BAR AND SECS IF SEC IS IN VIEWPORT**
 // consts for the bar animation:
 const colors = ['#FDF0E3', '#EBF4EC', '#F5F7F9']; // orange, green, lightgrey
 
-// add a class to a particular section
+// add a border to a particular section (which will be active section)
 const sectionViewed = document.getElementsByClassName('active-class');
 
 function activeSection(sec) {
@@ -74,12 +76,14 @@ function detectGo() {
       const corrAnchor = document.querySelector(`[data-sectionclass = ${sectionClass}]`); //detects which one is the corrisponding anchor
       const colorsIndex = item.target.getAttribute('data-index'); //used for giving the div the colors of the section
       if (item.isIntersecting) {
+        // change colors of active navbar item to highlight it
         const previousActive = navbar.querySelectorAll('h4');
         previousActive.forEach(head => {
           head.style.setProperty('background-color', 'white')
         });
         const anchorHead = corrAnchor.querySelector('h4');
         anchorHead.style.setProperty('background', colors[colorsIndex]);
+        // change class of the active section to highlight it
         const elmn = document.querySelector(`.${sectionClass}`);
         activeSection(elmn);
         // uncomment following to see section position:
@@ -101,7 +105,7 @@ function detectGo() {
 
 detectGo();
 
-// ** MAKE SECTIONS COLLAPSIBLE
+// ** MAKE SECTIONS COLLAPSIBLE**
 secs.forEach((section) => {
   const collApse = section.querySelector('.collapse');
   collApse.addEventListener('click', () => {
@@ -115,8 +119,9 @@ secs.forEach((section) => {
   });
 });
 
-// hide two posts in responsive mode
+// hide two posts in responsive mode for better user experience
 const postButton = document.getElementById('button-posts');
+
 postButton.addEventListener('click', () => {
   if (postButton.innerHTML === 'more') {
     postButton.innerHTML = 'less'
@@ -124,19 +129,21 @@ postButton.addEventListener('click', () => {
     postButton.innerHTML = 'more'
   }
   collPosts = secs[0].querySelectorAll('div[data-collapse = "killme"]');
-  collPosts.forEach(post => {post.classList.toggle("collapsible-post")});// collPost.classList.toggle('collapsed');
+  collPosts.forEach(post => {
+    post.classList.toggle("collapsible-post")
+  }); // collPost.classList.toggle('collapsed');
 });
 
-// ** CREATES A BUTTON TO GO UP TO PAGE TOP
+// ** CREATES A BUTTON TO GO UP TO PAGE TOP**
 const topBut = document.getElementById('go-top');
+
 window.onscroll = function() {
   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
     topBut.style = 'display: inline-block;';
   }
 };
 
-
-//** ADD AN EVENT WHICH DETECTS IF USER IS SCROLLING
+//** ADD AN EVENT WHICH DETECTS IF USER IS SCROLLING**
 let scrollTimer = '';
 
 function changeStyle() {
@@ -156,6 +163,5 @@ document.addEventListener('mousemove', (event) => {
     navID.style = 'position: sticky; top: 0px;';
   }
 });
-
 
 changeBarBe();
